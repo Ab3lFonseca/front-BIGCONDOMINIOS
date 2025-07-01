2f42a6e5cef771db04288f1d580ac4b3470fc453
Bruno Cristiano <bruno120505@gmail.com>
2025-07-01 20:24:34 -0300

    atualizado

--- src/controllers/gestaoMultasController.js
+++ src/controllers/gestaoMultasController.js
@@ -39,7 +39,7 @@ function adicionarMulta(event) {
     event.preventDefault();
 
     const multa = {
-        morador: parseInt(document.getElementById("morador").value),
+        moradorId: parseInt(document.getElementById("morador").value),
         valor: parseFloat(document.getElementById("valor").value),
         dataOcorrencia: getLocalDateTimeString(),
         dataVencimento: new Date(document.getElementById("dataVencimento").value).toISOString().split(".")[0],
